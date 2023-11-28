29118f601aecc9ecb4248172b8cb9228458904f0
tuliodev <resendetulio51@gmail.com>
2023-11-28 17:57:29 -0300

    feature: add contracts, command-line and graphql comunication

--- rest_api/scripts/createProposal.js
+++ rest_api/scripts/createProposal.js
@@ -1,12 +1,12 @@
+require('dotenv').config();
 const { Command } = require('commander');
-const { Contract } = require('ethers');
+const { Contract, JsonRpcProvider } = require('ethers');
 const DAO_ABI = require('./helpers/constants').DAO_ABI;
 const { ethers } = require('hardhat');
 const program = new Command();
-require('dotenv').config();
 
 let proposalDescription;
-let proposalDeadline;
+let proposalTitle;
 
 program
   .name('createProposal')
@@ -15,11 +15,11 @@ program
 
 program.command('createProposal')
   .description('Create a proposal')
+  .argument('<title>', 'proposal title')
   .argument('<description>', 'proposal description')
-  .argument('<proposalTime>', 'proposal deadline')
-  .action((description, proposalTime) => {
+  .action((title, description) => {
+    proposalTitle = title
     proposalDescription = description;
-    proposalDeadline = proposalTime
   });
 
 program.parse();
@@ -27,24 +27,27 @@ program.parse();
 
 async function createProposal() {
     try {
-        const provider = new ethers.providers.JsonRpcProvider(process.env.API_URL)
+        const provider = new JsonRpcProvider(process.env.API_URL)
         const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
-    
+      
         const DAOContract = new Contract(process.env.CONTRACT_ADDRESS, DAO_ABI, signer)
-
+      
         const proposalsCalldatas = ["0x0123456789abcdef"];
 
         const proposalTargets = [process.env.TOKEN_ADDRESS];
         const proposalValues = [0];
- 
+
         const proposalTx = await DAOContract.propose(
             proposalTargets,
             proposalValues,
             proposalsCalldatas,
+            proposalTitle,
             proposalDescription
         )
 
+        console.log("Creating proposal....")
 
+    
         await proposalTx.wait();
 
         console.log("Proposal Created With Success")
